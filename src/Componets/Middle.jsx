4d474213eb09166f0068981349fb1c18847f7225
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2025-08-17 14:32:17 -0400

    added phone to middle section

--- src/Componets/Middle.jsx
+++ src/Componets/Middle.jsx
@@ -3,9 +3,13 @@ import React, {Component} from 'react';
 class Middle extends Component {
     render() {
         return (
-            <div className="bg-gradient-to-r from-[#663399] to-[#5D3FD3] h-150 relative h-screen flex items-start pt-22 pl-20 flex-col">
-                <h1 className = "text-white text-5xl font-extrabold tracking-wide leading-tight">GIVE YOUR BRAND THE<br />WEB DESIGN IT DESERVES</h1>
-                <h2 className = "text-white pt-10 pl-20">PROFESSIONAL WEB DESIGN & DEVELOPMENT</h2>
+            <div className="bg-gradient-to-r from-[#663399] to-[#5D3FD3] h-150 relative h-screen flex justify-between">
+                <div className="flex items-start pt-22 pl-20 flex-col">
+                    <h1 className = "text-white text-5xl font-extrabold tracking-wide leading-tight">GIVE YOUR BRAND THE<br />
+                    WEB DESIGN IT DESERVES</h1>
+                    <h2 className = "text-white pt-6 ">PROFESSIONAL WEB DESIGN & DEVELOPMENT</h2>
+                </div>
+                <img src="./iphone.png" alt="iphone logo" className="pr-25 pt-5 h-150 "/>
             </div>
         );
     }
