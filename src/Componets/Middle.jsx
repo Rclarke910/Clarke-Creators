57e0329e6a99d7fa260f4584953512d3193f5995
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2025-08-17 17:28:15 -0400

    added bullets to Middle.jsx

--- src/Componets/Middle.jsx
+++ src/Componets/Middle.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BsLightningFill } from "react-icons/bs";
 
 class Middle extends Component {
     render() {
@@ -7,7 +8,12 @@ class Middle extends Component {
                 <div className="flex items-start pt-22 pl-20 flex-col">
                     <h1 className = "text-white text-5xl font-extrabold tracking-wide leading-tight">GIVE YOUR BRAND THE<br />
                     WEB DESIGN IT DESERVES</h1>
-                    <h2 className = "text-white pt-6 ">PROFESSIONAL WEB DESIGN & DEVELOPMENT</h2>
+                    <h2 className = "text-white pt-6 text-2xl">PROFESSIONAL WEB DESIGN & DEVELOPMENT</h2>
+                    <div className='flex flex-col'>
+                        <p className='text-white pt-6 pl-4'>Get a website designed in under a week</p>
+                        <p className='text-white pt-6 pl-4'>Get a secure database and a real time analytics dashboard</p>
+                        <p className='text-white pt-6 pl-4'>Ongoing support and maintenance for future updates</p>
+                     </div>
                 </div>
                 <img src="./iphone.png" alt="iphone logo" className="pr-25 pt-5 h-150 "/>
             </div>
